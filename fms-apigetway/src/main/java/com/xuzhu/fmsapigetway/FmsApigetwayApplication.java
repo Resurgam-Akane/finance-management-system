3cee3c4247dd72d81f2480b5570005f0d3af44ec
Resurgam <xuzhu1996@hotmail.com>
2018-02-26 18:26:12 +0800

    add zuul

--- fms-apigetway/src/main/java/com/xuzhu/fmsapigetway/FmsApigetwayApplication.java
+++ fms-apigetway/src/main/java/com/xuzhu/fmsapigetway/FmsApigetwayApplication.java
@@ -1,17 +1,16 @@
-package com.xuzhu.fmsconfig;
+package com.xuzhu.fmsapigetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
-
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableZuulProxy
 @EnableDiscoveryClient
-public class FmsConfigApplication {
+public class FmsApigetwayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FmsConfigApplication.class, args);
+		SpringApplication.run(FmsApigetwayApplication.class, args);
 	}
 }
---
+++ fms-apigetway/src/test/java/com/xuzhu/fmsapigetway/FmsApigetwayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xuzhu.fmsapigetway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmsApigetwayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
