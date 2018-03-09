313287db2cc937f75fee5cfdf7f1db3e35ea0d31
Resurgam <xuzhu1996@hotmail.com>
2018-03-09 18:52:40 +0800

    some todo

---
+++ fms-auth-service/src/main/java/com/xuzhu/fmsauthservice/FmsAuthServiceApplication.java
@@ -0,0 +1,29 @@
+package com.xuzhu.fmsauthservice;
+
+import com.xuzhu.fmsauthservice.service.security.MongoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableResourceServer
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class FmsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsAuthServiceApplication.class, args);
+	}
+}
---
+++ fms-statistic-service/src/main/java/com/xuzhu/fmsstatisticservice/FmsStatisticServiceApplication.java
@@ -0,0 +1,16 @@
+package com.xuzhu.fmsstatisticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+public class FmsStatisticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsStatisticServiceApplication.class, args);
+	}
+}
